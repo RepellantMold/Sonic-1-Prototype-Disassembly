3b7710e5e8a03bbd52f6a748f6388220ecc0811d
BetaFilter <76820143+BetaFilter@users.noreply.github.com>
2021-05-17 18:05:25 -0400

    Add files via upload

---
+++ Unknown/Unk_3898.asm
@@ -0,0 +1,72 @@
+unk13898:       dc.b   4
+                dc.b   0
+                dc.b   6
+                dc.b   0
+                dc.b   6
+                dc.b $20
+                dc.b   6
+                dc.b $24 ; $
+                dc.b   6
+                dc.b $64 ; d
+                dc.b   6
+                dc.b $66 
+                dc.b   6
+                dc.b   0
+                dc.b   8
+                dc.b $20
+                dc.b  $A
+                dc.b $64 ; d
+                dc.b  $A
+                dc.b $68 ; h
+                dc.b  $A
+                dc.b $A6
+                dc.b  $A
+                dc.b $AA
+                dc.b   8
+                dc.b   0
+                dc.b  $C
+                dc.b $42 ; B
+                dc.b  $E
+                dc.b $86
+                dc.b  $E
+                dc.b $CA
+                dc.b  $E
+                dc.b $EC
+                dc.b  $E
+                dc.b $EE
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b $20
+                dc.b   6
+                dc.b $20
+                dc.b   6
+                dc.b $20
+                dc.b   8
+                dc.b $64 ; d
+                dc.b   6
+                dc.b $66 
+                dc.b   4
+                dc.b $20
+                dc.b   6
+                dc.b $20
+                dc.b   8
+                dc.b $42 ; B
+                dc.b   8
+                dc.b $42 ; B
+                dc.b  $A
+                dc.b $86
+                dc.b  $A
+                dc.b $AA
+                dc.b   6
+                dc.b $20
+                dc.b   8
+                dc.b $42 ; B
+                dc.b  $A
+                dc.b $64 ; d
+                dc.b  $C
+                dc.b $86
+                dc.b  $E
+                dc.b $A8
+                dc.b  $E
+                dc.b $EE
---
+++ Unknown/Unk_38E0.asm
@@ -0,0 +1,210 @@
+unk138E0:       dc.b  $E
+                dc.b $EA
+                dc.b  $E
+                dc.b $E0
+                dc.b  $A
+                dc.b $A0
+                dc.b   8
+                dc.b $80
+                dc.b   6
+                dc.b $60 
+                dc.b   4
+                dc.b $40 ; @
+                dc.b  $E
+                dc.b $E0
+                dc.b  $A
+                dc.b $A0
+                dc.b   4
+                dc.b $40 ; @
+                dc.b  $A
+                dc.b $A0
+                dc.b  $A
+                dc.b $A0
+                dc.b  $A
+                dc.b $A0
+                dc.b   8
+                dc.b $60 
+                dc.b   8
+                dc.b $60 
+                dc.b   8
+                dc.b $60 
+                dc.b   6
+                dc.b $40 ; @
+                dc.b   6
+                dc.b $40 ; @
+                dc.b   6
+                dc.b $40 ; @
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b  $A
+                dc.b $EC
+                dc.b   6
+                dc.b $EA
+                dc.b   4
+                dc.b $C6
+                dc.b   2
+                dc.b $A4
+                dc.b   0
+                dc.b $82
+                dc.b   0
+                dc.b $60 
+                dc.b   6
+                dc.b $EA
+                dc.b   4
+                dc.b $C6
+                dc.b   0
+                dc.b $60 
+                dc.b   4
+                dc.b $C6
+                dc.b   4
+                dc.b $C6
+                dc.b   4
+                dc.b $C6
+                dc.b   4
+                dc.b $84
+                dc.b   4
+                dc.b $84
+                dc.b   4
+                dc.b $84
+                dc.b   4
+                dc.b $42 ; B
+                dc.b   4
+                dc.b $42 ; B
+                dc.b   4
+                dc.b $42 ; B
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b  $E
+                dc.b $CC
+                dc.b  $E
+                dc.b $8A
+                dc.b  $C
+                dc.b $68 ; h
+                dc.b  $A
+                dc.b $46 
+                dc.b   8
+                dc.b $24 ; $
+                dc.b   6
+                dc.b   2
+                dc.b  $E
+                dc.b $8A
+                dc.b  $C
+                dc.b $68 ; h
+                dc.b   6
+                dc.b   2
+                dc.b  $C
+                dc.b $68 ; h
+                dc.b  $C
+                dc.b $68 ; h
+                dc.b  $C
+                dc.b $68 ; h
+                dc.b   8
+                dc.b $46 
+                dc.b   8
+                dc.b $46 
+                dc.b   8
+                dc.b $46 
+                dc.b   6
+                dc.b $24 ; $
+                dc.b   6
+                dc.b $24 ; $
+                dc.b   6
+                dc.b $24 ; $
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b  $A
+                dc.b $EC
+                dc.b   8
+                dc.b $CA
+                dc.b   6
+                dc.b $A8
+                dc.b   4
+                dc.b $86
+                dc.b   2
+                dc.b $64 ; d
+                dc.b   0
+                dc.b $42 ; B
+                dc.b   8
+                dc.b $CA
+                dc.b   6
+                dc.b $A8
+                dc.b   0
+                dc.b $42 ; B
+                dc.b   6
+                dc.b $A8
+                dc.b   6
+                dc.b $A8
+                dc.b   6
+                dc.b $A8
+                dc.b   6
+                dc.b $84
+                dc.b   6
+                dc.b $84
+                dc.b   6
+                dc.b $84
+                dc.b   4
+                dc.b $42 ; B
+                dc.b   4
+                dc.b $42 ; B
+                dc.b   4
+                dc.b $42 ; B
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b  $E
+                dc.b $EC
+                dc.b  $C
+                dc.b $CA
+                dc.b  $A
+                dc.b $A8
+                dc.b   8
+                dc.b $86
+                dc.b   6
+                dc.b $64 ; d
+                dc.b   4
+                dc.b $42 ; B
+                dc.b  $C
+                dc.b $CA
+                dc.b  $A
+                dc.b $A8
+                dc.b   4
+                dc.b $42 ; B
+                dc.b  $A
+                dc.b $A8
+                dc.b  $A
+                dc.b $A8
+                dc.b  $A
+                dc.b $A8
+                dc.b   8
+                dc.b $64 ; d
+                dc.b   8
+                dc.b $64 ; d
+                dc.b   8
+                dc.b $64 ; d
+                dc.b   6
+                dc.b $42 ; B
+                dc.b   6
+                dc.b $42 ; B
+                dc.b   6
+                dc.b $42 ; B
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b   4
+                dc.b   0
