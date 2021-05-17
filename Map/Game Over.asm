25d9b4fa8a5e07d9b37c0e7a5e1ac39e99633a2f
BetaFilter <76820143+BetaFilter@users.noreply.github.com>
2021-05-17 17:56:39 -0400

    Add files via upload

---
+++ Map/Game Over.asm
@@ -0,0 +1,26 @@
+Mapobj39:       dc.b   0
+                dc.b   4
+                dc.b   0
+                dc.b  $F
+                dc.b   2
+                dc.b $F8
+                dc.b  $D
+                dc.b   0
+                dc.b   0
+                dc.b $B8
+                dc.b $F8
+                dc.b  $D
+                dc.b   0
+                dc.b   8
+                dc.b $D8
+                dc.b   2
+                dc.b $F8
+                dc.b  $D
+                dc.b   0
+                dc.b $14
+                dc.b   8
+                dc.b $F8
+                dc.b  $D
+                dc.b   0
+                dc.b  $C
+                dc.b $28 ; (
---
+++ Map/Seesaw.asm
@@ -0,0 +1,66 @@
+Map_Seesaw:     dc.b   0
+                dc.b   8
+                dc.b   0
+                dc.b $2C ; ,
+                dc.b   0
+                dc.b   8
+                dc.b   0
+                dc.b $2C ; ,
+                dc.b   7
+                dc.b $D4
+                dc.b   6
+                dc.b   0
+                dc.b   0
+                dc.b $D3
+                dc.b $DC
+                dc.b   6
+                dc.b   0
+                dc.b   6
+                dc.b $E3
+                dc.b $E4
+                dc.b   4
+                dc.b   0
+                dc.b  $C
+                dc.b $F3
+                dc.b $EC
+                dc.b  $D
+                dc.b   0
+                dc.b  $E
+                dc.b $F3
+                dc.b $FC
+                dc.b   8
+                dc.b   0
+                dc.b $16
+                dc.b $FB
+                dc.b $F4
+                dc.b   6
+                dc.b   0
+                dc.b   6
+                dc.b $13
+                dc.b $FC
+                dc.b   5
+                dc.b   0
+                dc.b $19
+                dc.b $23 ; #
+                dc.b   4
+                dc.b $E6
+                dc.b  $A
+                dc.b   0
+                dc.b $1D
+                dc.b $D0
+                dc.b $E6
+                dc.b  $A
+                dc.b   0
+                dc.b $23 ; #
+                dc.b $E8
+                dc.b $E6
+                dc.b  $A
+                dc.b   8
+                dc.b $23 ; #
+                dc.b   0
+                dc.b $E6
+                dc.b  $A
+                dc.b   8
+                dc.b $1D
+                dc.b $18
+                dc.b   0
