f60a0faed7e02b84419a436df8f005bd615ffea0
BetaFilter <76820143+BetaFilter@users.noreply.github.com>
2021-05-17 18:00:40 -0400

    Add files via upload

---
+++ Ani/Obj35.asm
@@ -0,0 +1,8 @@
+unk92D4:        dc.b   0
+                dc.b   2
+                dc.b   5
+                dc.b   0
+                dc.b $20
+                dc.b   1
+                dc.b $21 ; !
+                dc.b $FF
---
+++ Ani/Yadrin.asm
@@ -0,0 +1,4 @@
+Ani_Yadrin:    dc.w byte_D40C-*
+               dc.w byte_D410-Ani_Yadrin
+byte_D40C:     dc.b 7, 0, $FF, 0
+byte_D410:     dc.b 7, 0, 3, 1, 4, 0, 3, 2, 5, $FF
