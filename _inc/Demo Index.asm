2cb52c908775b703269ef12abaa2d25cfe4222f7
BetaFilter <76820143+BetaFilter@users.noreply.github.com>
2021-05-17 17:55:54 -0400

    Add files via upload

---
+++ _inc/Demo Index.asm
@@ -0,0 +1,10 @@
+DemoIndex:      dc.l Demo_GHZ
+                dc.l Demo_GHZ
+                dc.l Demo_GHZ
+                dc.l Demo_SS
+                dc.l Demo_MZ
+                dc.l Demo_MZ
+                dc.l Demo_SZ
+                dc.b 0, $8B, 8, $37, 0, $42, 8, $5C, 0, $6A, 8, $5F, 0, $2F, 8, $2C
+                dc.b 0, $21, 8, 3, $28, $30, 8, 8, 0, $2E, 8, $15, 0, $F, 8, $46
+                dc.b 0, $1A, 8, $FF, 8, $CA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
