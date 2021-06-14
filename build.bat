e61bbabc24f0793fcb6333c2015f948bd962d9ee
Jacob T. Andersen <jacobjaretts@gmail.com>
2021-06-14 08:52:54 -0700

    updating build script slightly

--- build.bat
+++ build.bat
@@ -2,17 +2,17 @@
 
 echo Build Started
 
-asm68k /k /p /o ae- sonic1proto.asm, s1built.bin
-IF NOT EXIST s1built.bin goto LABLERR
-rompad.exe s1built.bin
-fixheadr.exe s1built.bin
+asm68k /k /p /q /o ae- sonic1proto.asm, s1pbuilt.bin, ,output.lst >.log
+IF NOT EXIST s1pbuilt.bin goto LABLERR
+rompad.exe s1pbuilt.bin
+fixheadr.exe s1pbuilt.bin
 
 echo Build Successful!
 
 goto LABLDONE
 
 :LABLERR
+type .log
 echo There was a problem building. Please check through the error message and fix the error.
-pause
 :LABLDONE
 pause
